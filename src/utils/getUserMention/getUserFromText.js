669c3c45a9c35ba8de0e56c2beaecca68dcf1d5a
Mateus Andriola <mateusandriola18@gmail.com>
2021-09-20 12:35:55 -0300

    feat(message.events): insert validation for is season command, update getUserFromText

--- src/utils/getUserMention/getUserFromText.js
+++ src/utils/getUserMention/getUserFromText.js
@@ -1,7 +1,9 @@
 export function getUserFromText(client, text) {
   const matchRegex = /((.)+(#)([0-9]{4}))|([0-9]{18})/;
-  const userTagOrId = text.trim()?.match(matchRegex)[0] || text.id;
   console.log(text);
+  const userTagOrId = text.trim()?.match(matchRegex)
+    ? text.trim()?.match(matchRegex)[0]
+    : text.id;
   const resultUser = client.users.cache.find((user) =>
     [user.tag, user.id].includes(userTagOrId)
   );
