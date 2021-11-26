132487db995624255b7d397e804404b908e468b6
tauzxd <tauasanto101@gmail.com>
2021-11-25 22:24:04 -0300

    feat(listening to ban event): now ban event has regex for reason, date and author divisions

--- src/events/client/guildBanAdd.event.js
+++ src/events/client/guildBanAdd.event.js
@@ -1,5 +1,7 @@
+/* eslint-disable prettier/prettier */
 import Discord from 'discord.js';
 import Colors from '../../utils/layoutEmbed/colors.js';
+import { parseDateForDiscord } from '../../utils/TimeMessageConversor/parseDateForDiscord.js';
 
 export default {
   name: 'guildBanAdd',
@@ -9,18 +11,57 @@ export default {
     const channelLog = client.channels.cache.get(
       guildIdDatabase.get('channel_log')
     );
-    const usersBanneds = await guild.fetchBans();
 
+    const usersBanneds = await guild.fetchBans();
     const userBanned = usersBanneds.find((x) => x.user.id === user.id);
+    const dataValidation =
+      /(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}).(\d{3})Z/;
+    const autorValidation = /(Punido por )(.*)\n/;
+
+    const reasonFull = userBanned.reason;
+
+    const userDataBanned = dataValidation.test(reasonFull)
+      ? parseDateForDiscord(reasonFull.match(dataValidation)[0])
+      : '`<Data não especificada, utilize o ballebot sempre que for banir para ter essa função>`';
+
+    const userAutorBanned = autorValidation.test(reasonFull)
+      ? reasonFull.match(autorValidation)[0].replace(/(Punido por )|(\n)/g, '')
+      : `<sem autor>`;
+
+    const descriptionBan = userBanned.reason
+      ? userBanned.reason
+        .replace(' — Data: ', '')
+        .replace(dataValidation, '')
+        .replace(autorValidation, '')
+        .replace('— Motivo: ', '')
+      : '<Descrição ou motivo não especificado>';
+
+
     channelLog?.send(
       user,
       new Discord.MessageEmbed()
         .setColor(Colors.pink_red)
         .setThumbnail(user.displayAvatarURL({ dynamic: true }))
-        .setTitle(`Usuário ${user.tag} foi banido manualmente!`)
-        .setDescription(`**Descrição: ** \n${userBanned.reason} \n`)
+        .setTitle(`Usuário ${user.tag} foi banido!`)
+        .setDescription(
+          `**Data: ${userDataBanned}**\n**Punido por: ${userAutorBanned}**\n**Motivo: **${descriptionBan} \n`
+        )
         .setFooter(`ID do usuário: ${user.id}`)
         .setTimestamp()
-    );
+    ).catch(() => {
+
+      const buffer = Buffer.from(
+        reasonFull
+      );
+      const attachment = new Discord.MessageAttachment(
+        buffer,
+        `ban_of_${user.tag}.txt`
+      );
+      channelLog?.send(
+        `${user} O usuário possui um motivo muito grande e por esse motivo enviei um arquivo para você ver todo o motivo`,
+        attachment
+      );
+    });
+
   },
 };
