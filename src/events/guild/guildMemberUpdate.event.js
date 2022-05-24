184f7e830514b325cbfbabb8e65afca20abd4e9f
JVOPINHO <jvopinho.dev@gmail.com>
2022-05-24 10:40:55 -0300

    fix(mute system): removing some utils and events related with old mute system

--- src/events/guild/guildMemberUpdate.event.js
+++
@@ -1,9 +0,0 @@
-import { removeMuteRole } from '../../utils/verifications/muteVerify/removeMuteRole.js';
-
-export default {
-  name: 'guildMemberUpdate',
-  once: false,
-  run: async (client, oldMember, newMember) => {
-    await removeMuteRole(client, oldMember, newMember);
-  },
-};
