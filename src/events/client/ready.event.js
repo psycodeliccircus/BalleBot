81c0793d3c759807dbeed2c1650764ba975346a4
João Pinho <jvopinho.dev@gmail.com>
2022-06-01 13:43:51 -0300

    Fixing bug

--- src/events/client/ready.event.js
+++ src/events/client/ready.event.js
@@ -1,14 +1,9 @@
 import { statusActivity } from '../../assets/statusActivity.js';
-import { setIntervalRemoveMute } from '../../services/setTimeoutMute/setTimeOutMute.js';
 
 export default {
   name: 'ready',
   once: false,
   run: (client) => {
-    setInterval(async () => {
-      setIntervalRemoveMute(client);
-    }, 1000);
-
     statusActivity(client);
     console.log(`Logged as ${client.user.tag}`);
   },
