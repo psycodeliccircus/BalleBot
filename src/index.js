99ef04f33ebf8610bfd547dca3140c9452fb5a3e
renildomarcio <krsolucoesweb@gmail.com>
2023-02-04 08:46:36 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 import 'dotenv/config';
 
-import { Client, Intents } from 'discord.js';
-import { Database } from 'quick.db';
+import { Client, Intents, Permissions } from 'discord.js';
+import db from 'quick.db';
 import eventHandler from './events/Events.handler.js';
 import commandHandler from './commands/Command.handler.js';
 
 const { TOKEN } = process.env;
 
-const intents = new Intents()
-  .add([
-    Intents.FLAGS.GUILD_PRESENCES,
-    Intents.FLAGS.GUILD_MEMBERS,
-    Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILD_MESSAGES,
-    Intents.FLAGS.GUILD_BANS,
-    Intents.FLAGS.DIRECT_MESSAGES,
-    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-  ]);
+const intents = new Intents();
+
+intents.add(
+  Intents.FLAGS.GUILD_PRESENCES,
+  Intents.FLAGS.GUILD_MEMBERS,
+  Intents.FLAGS.GUILDS,
+  Intents.FLAGS.GUILD_MESSAGES,
+  Intents.FLAGS.GUILD_BANS,
+  Intents.FLAGS.DIRECT_MESSAGES,
+  Intents.FLAGS.GUILD_MESSAGE_REACTIONS
+);
 
 const client = new Client({
   restRequestTimeout: 60000,
@@ -24,9 +25,9 @@ const client = new Client({
   partials: ['CHANNEL'],
 });
 
-client.events = eventHandler(client);
-client.commands = commandHandler();
-client.database = new Database();
-client.permission = new Permissions(104324673n);
+client.Events = eventHandler(client);
+client.Commands = commandHandler();
+client.Database = db;
+client.Permission = new Permissions(104324673n);
 
 client.login(TOKEN);
