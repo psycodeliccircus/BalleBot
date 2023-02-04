0f836016ddca5ac2385b2dc3e99d11d8c62146aa
renildomarcio <krsolucoesweb@gmail.com>
2023-02-04 08:49:50 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ import commandHandler from './commands/Command.handler.js';
 
 const { TOKEN } = process.env;
 
+// Check if token is set
+if (!TOKEN) {
+  throw new Error('Discord token not provided.');
+}
+
 const intents = new Intents()
   .add([
     Intents.FLAGS.GUILD_PRESENCES,
@@ -24,9 +29,11 @@ const client = new Client({
   partials: ['CHANNEL'],
 });
 
+// Attach event handler to client instance
 client.events = eventHandler(client);
 client.commands = commandHandler();
 client.database = new Database();
 client.permission = new Permissions(104324673n);
 
+// Login to Discord API
 client.login(TOKEN);
